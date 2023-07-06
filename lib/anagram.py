676a1949dd944e690cd6d66cab542eae2843d1cb
maddox earles <mdxear@gmail.com>
2023-07-06 11:36:41 -0600

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        anagram_list = []
+        for anagram in anagrams:
+            if(sorted(anagram) == sorted(self.word)):
+                anagram_list.append(anagram)
+        return anagram_list
+    
